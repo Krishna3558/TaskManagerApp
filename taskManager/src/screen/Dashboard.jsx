c06246b9f1ce5f9dd34839e9b570401707eb893f
Krishna3558 <rajeevmishra6066@gmail.com>
2025-01-03 21:43:34 +0530

    Update Dashboard.jsx

--- taskManager/src/screen/Dashboard.jsx
+++ taskManager/src/screen/Dashboard.jsx
@@ -1,13 +1,8 @@
 import React from 'react'
-import Navbar from '../Components/navbar'
-import Homepage from '../Components/Homepage'
-import { TaskProvider } from '../context/task'
 
 function Dashboard() {
   return (
     <div>
-        <Navbar />
-        <Homepage/>
     </div>
   )
 }
